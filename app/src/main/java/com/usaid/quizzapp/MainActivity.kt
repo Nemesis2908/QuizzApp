91fe7a08f9846141f6f099e26b68cb7be7e1caf9
lab_services_student <Musaid7860@gmail.com>
2025-05-20 16:13:24 +0200

    main Activity code done

--- app/src/main/java/com/usaid/quizzapp/MainActivity.kt
+++ app/src/main/java/com/usaid/quizzapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.usaid.quizzapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }//end of ViewCompat
+        //code starts here
+        val btnStartQuiz = findViewById<Button>(R.id.btnStart)
+
+        btnStartQuiz.setOnClickListener{
+            //start the quiz
+            val Intent = Intent(this, quizzActivity::class.java)
+            startActivity(Intent)
         }
-    }
-}
+    }//end of OnCreate
+}//end of MainActivity
