9547fa91f25e6b8dd29665a8c442a79db23a91d0
lab_services_student <Musaid7860@gmail.com>
2025-05-20 21:47:02 +0200

    results activity code done

--- app/src/main/java/com/usaid/quizzapp/ResultsActivity.kt
+++ app/src/main/java/com/usaid/quizzapp/ResultsActivity.kt
@@ -1,6 +1,8 @@
 package com.usaid.quizzapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,16 @@ class ResultsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }//end of ViewCompat
+        //Code goes here
+        val tvScore = findViewById<TextView>(R.id.Score)
+        val btnExit = findViewById<Button>(R.id.btnexit)
+        //code logic
+        //get the score from the intent
+        val score = intent.getIntExtra("score", 0)
+        //display the score in the textview
+        btnExit.setOnClickListener {
+            finish()
         }
-    }
-}
+    }//end of onCreate
+}//end of ResultActivity
